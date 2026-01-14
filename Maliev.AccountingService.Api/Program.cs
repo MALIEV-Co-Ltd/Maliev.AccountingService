c1736dedcc7f0ccff68dd4634d2b1662c75c95a4
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-01-14 18:20:23 +0700

    chore: cleanup bootstrap logging and suppress CA1848

--- Maliev.AccountingService.Api/Program.cs
+++ Maliev.AccountingService.Api/Program.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CA1848 // For improved performance, use the LoggerMessage delegates
 using Maliev.AccountingService.Api.Services;
 using Maliev.AccountingService.Data.Data;
 using Maliev.Aspire.ServiceDefaults;
